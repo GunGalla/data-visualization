1657a6d2f0024092b58dcd834ac1fd90c57d92a5
Iaroslav <GunGalla@gmail.com>
2023-01-13 10:47:10 +0200

    Refactor project structure

--- examples/mpl_sqares.py
+++ examples/mpl_sqares.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Square visualization."""
 import matplotlib.pyplot as plt
 
@@ -5,5 +6,11 @@ squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
 ax.plot(squares)
 
-if __name__ == '__main__':
+
+def main():
+    """Start the visualization"""
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
