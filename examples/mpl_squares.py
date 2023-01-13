87f39b0aac42c5ac23fa094fccc2e6018902b935
Iaroslav <GunGalla@gmail.com>
2023-01-13 11:45:29 +0200

    Add info to the graph

--- examples/mpl_squares.py
+++ examples/mpl_squares.py
@@ -2,9 +2,17 @@
 """Square visualization."""
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Diagram and axis titles
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set axis font
+ax.tick_params(axis='both', labelsize=14)
 
 
 def main():
