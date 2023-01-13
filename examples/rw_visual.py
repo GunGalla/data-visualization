bb741dc1e905bd316ccb3fe8ec2839e3b0f4ef3c
Iaroslav <GunGalla@gmail.com>
2023-01-13 14:29:30 +0200

    Enlight start and finish points, remove axises

--- examples/rw_visual.py
+++ examples/rw_visual.py
@@ -14,6 +14,16 @@ while True:
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
                cmap=plt.cm.Greens, edgecolors='none', s=10)
+
+    # Enlight first and final point
+    ax.scatter(0, 0, c='red', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='orange',
+               edgecolors='none', s=100)
+
+    # Remove axis
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
