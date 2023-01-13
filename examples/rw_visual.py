1290669d2c2c8fc5bb896ab17ef8beda5f644637
Iaroslav <GunGalla@gmail.com>
2023-01-13 13:54:19 +0200

    Add walk repetance feature

--- examples/rw_visual.py
+++ examples/rw_visual.py
@@ -3,12 +3,17 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Creating of random walk object
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # Creating of random walk object
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Set points on diagram
-plt.style.use('seaborn-v0_8-darkgrid')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=10)
-plt.show()
+    # Set points on diagram
+    plt.style.use('seaborn-v0_8-darkgrid')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=10)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
