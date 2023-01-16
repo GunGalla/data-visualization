1d0a22f5b105b08819b4418fd5eb75cfcdf08ae8
Iaroslav <GunGalla@gmail.com>
2023-01-16 16:18:12 +0200

    Add each repo desctiption on diagram

--- chapter-17/python_repos_visual.py
+++ chapter-17/python_repos_visual.py
@@ -13,16 +13,21 @@ print(f'Status code: {r.status_code}')
 # Save answer in variable
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
 
 # Visualization
 data = [{
     'type': 'bar',
     'x': repo_names,
     'y': stars,
+    'hovertext': labels,
     'marker': {
         'color': 'green',
         'line': {'width':1.5, 'color': 'grey'},
