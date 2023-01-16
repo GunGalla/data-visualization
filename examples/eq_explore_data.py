d6d45085143c829c63cc9f78bde3bd5a0c61f21b
Iaroslav <GunGalla@gmail.com>
2023-01-16 13:42:43 +0200

    Put eartquakes on the world map

--- examples/eq_explore_data.py
+++ examples/eq_explore_data.py
@@ -1,10 +1,30 @@
 """Earthquake data visualization."""
 import json
 
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
 # Research data structure
 filename = '/home/gungalla/Work/data-visualization/data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+# Put data on the map
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
 
 readable_file = '/home/gungalla/Work/data-visualization/data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
