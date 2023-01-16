fb4956e3ef8f03c0e195fd16ea2835f3d0d239d0
Iaroslav <GunGalla@gmail.com>
2023-01-16 12:49:21 +0200

    Add min temperature info on diagram

--- examples/sitka_highs.py
+++ examples/sitka_highs.py
@@ -4,27 +4,29 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = '/home/gungalla/Work/data-visualization/data/sitka_weather_07-2018_simple.csv'
+filename = '/home/gungalla/Work/data-visualization/data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     # Reads MAX temperature
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
-    print(highs)
+        lows.append(low)
 
 # Put data on diagram
 plt.style.use('seaborn-v0_8-dark-palette')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='green')
+plt.plot(dates, lows, c='blue')
 
 # Diagram formatting
-plt.title('Daily high temperatures, July 2018', fontsize=24)
+plt.title('Daily high and low temperatures, 2018', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
