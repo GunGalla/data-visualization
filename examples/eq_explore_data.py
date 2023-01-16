34efea8dd44adeb056ab5036ba341ba30f29efa7
Iaroslav <GunGalla@gmail.com>
2023-01-16 13:55:55 +0200

    Markers color improvement. Add Magnitude colorbar

--- examples/eq_explore_data.py
+++ examples/eq_explore_data.py
@@ -5,7 +5,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Research data structure
-filename = '/home/gungalla/Work/data-visualization/data/eq_data_1_day_m1.json'
+filename = '/home/gungalla/Work/data-visualization/data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -26,6 +26,10 @@ data = [{
     'lat': lats,
     'marker': {
         'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
     },
 }]
 my_layout = Layout(title='Global Earthquakes')
